d63d5e05d0f40199ca3b07270d63a540b3ac4aa4
Peter <ghdtjrgus333@naver.com>
2023-05-18 15:59:19 +0900

    feat: weatherDTO 및 domain weather 객체 생성

--- Clima/Clima/Sources/Data/DataMapping/WeatherResponseDTO.swift
+++ Clima/Clima/Sources/Data/DataMapping/WeatherResponseDTO.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+struct WeatherTotalDTO: Codable {
+    let cityName: String
+    let weather: [WeatherDTO]
+    let main: MainInformationDTO
+    
+    enum CodingKeys: String, CodingKey {
+        case cityName = "name"
+        case weather, main
+    }
+    
+    func toDomain() -> Weather {
+        guard let weather = weather.first else {
+            return Weather(
+                conditionID: 200,
+                cityName: "Seoul",
+                temperature: 0.0
+            )
+        }
+        return Weather(
+            conditionID: weather.id,
+            cityName: cityName,
+            temperature: main.temperature
+        )
+    }
+}
+
+struct MainInformationDTO: Codable {
+    let temperature : Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temperature = "temp"
+    }
+}
+
+struct WeatherDTO: Codable {
+    let id: Int
+    let currentWeather: String
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, description
+        case currentWeather = "main"
+    }
+}
