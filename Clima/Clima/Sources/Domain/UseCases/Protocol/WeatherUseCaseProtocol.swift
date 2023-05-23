2c102cae01cfa2332c6745e19b3a64ef398b30e2
Peter <ghdtjrgus333@naver.com>
2023-05-23 11:07:05 +0900

    init: usecase, extension 파일 생성

---
+++ Clima/Clima/Sources/Domain/UseCases/Protocol/WeatherUseCaseProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherUseCaseProtocol.swift
+//  Clima
+//
+//  Created by Sh Hong on 2023/05/22.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Clima/Sources/Util/Extension.swift
@@ -0,0 +1,44 @@
+//
+//  Extension.swift
+//  Clima
+//
+//  Created by Sh Hong on 2023/05/22.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension ObservableType {
+    func asResult() -> Observable<Result<Element, Error>> {
+        return self.map { .success($0) }
+            .catch { .just(.failure($0)) }
+    }
+}
+
+public extension Reactive where Base: UIViewController {
+    var viewDidLoad: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
+        return ControlEvent(events: source)
+    }
+    
+    var viewWillAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewWillAppear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+    
+    var viewDidAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+    
+    var viewWillDisappear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewWillDisappear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+    var viewDidDisappear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidDisappear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+}
